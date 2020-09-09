a5a8c9fced6000ce4a34880cf6e3b857b403d71f
Subhojit paul <subhojit.paul@zensar.com>
2020-09-10 00:09:16 +0530

    Add yeild and multiDimensional Array

---
+++ YeildAssignment/YeildAssignment/MultiDimenArrayDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeildAssignment
+{
+    class MultiDimenArrayDemo
+    {
+        static void Main()
+        {
+            int[,] arr = new int[3, 3];//declaration of 2D array  
+            arr[0, 1] = 15;//initialization  
+            arr[1, 1] = 20;
+            arr[2, 0] = 18;
+            //int[,] arr = { { 18, 28, 30 }, { 41, 25, 6 }, { 0, 78, 19 } };//declaration and initialization  
+
+            //traversal  
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();//new line at each row  
+            }
+        }
+    }
+}
---
+++ YeildAssignment/YeildAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YeildAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var i in GetData())
+            {
+                Console.WriteLine(i.ToString());
+            }
+            Console.ReadLine();
+        }
+        static IEnumerable<string> GetData()
+        {
+            yield return "A";
+            yield return "B";
+            yield return "C";
+            //yield break;
+            yield return "D";
+            yield return "E";
+        }
+    }
+}
+    
+
